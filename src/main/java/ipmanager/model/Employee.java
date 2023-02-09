1948810564e6993274784a67b29a0e578a6ced1c
Leonardo José Nunes <leonardo.ferax@gmail.com>
2023-02-09 14:46:33 -0300

    Add Employee and Ip model.

---
+++ src/main/java/ipmanager/model/Employee.java
@@ -0,0 +1,69 @@
+package ipmanager.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long employeeId;
+
+	private String firstName;
+
+	private String lastName;
+
+	private String email;
+
+	@ManyToMany
+	private List<Ip> ip;
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(String firstName, String lastName, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public List<Ip> getIp() {
+		return ip;
+	}
+
+	public void setIp(List<Ip> ip) {
+		this.ip = ip;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
---
+++ src/main/java/ipmanager/model/Ip.java
@@ -0,0 +1,154 @@
+package ipmanager.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Ip {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long ipAddressId;
+
+	private String status;
+	private String country;
+	private String countryCode;
+	private String region;
+	private String regionName;
+	private String city;
+	private String zip;
+	private Double lat;
+	private Double lon;
+	private String timezone;
+	private String isp;
+	private String org;
+	private String query;
+
+	@ManyToMany
+	private List<Employee> employee;
+
+	public Long getIpAddressId() {
+		return ipAddressId;
+	}
+
+	public void setIpAddressId(Long ipAddressId) {
+		this.ipAddressId = ipAddressId;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public String getRegionName() {
+		return regionName;
+	}
+
+	public void setRegionName(String regionName) {
+		this.regionName = regionName;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public Double getLat() {
+		return lat;
+	}
+
+	public void setLat(Double lat) {
+		this.lat = lat;
+	}
+
+	public Double getLon() {
+		return lon;
+	}
+
+	public void setLon(Double lon) {
+		this.lon = lon;
+	}
+
+	public String getTimezone() {
+		return timezone;
+	}
+
+	public void setTimezone(String timezone) {
+		this.timezone = timezone;
+	}
+
+	public String getIsp() {
+		return isp;
+	}
+
+	public void setIsp(String isp) {
+		this.isp = isp;
+	}
+
+	public String getOrg() {
+		return org;
+	}
+
+	public void setOrg(String org) {
+		this.org = org;
+	}
+
+	public String getQuery() {
+		return query;
+	}
+
+	public void setQuery(String query) {
+		this.query = query;
+	}
+
+	public List<Employee> getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(List<Employee> employee) {
+		this.employee = employee;
+	}
+
+}
