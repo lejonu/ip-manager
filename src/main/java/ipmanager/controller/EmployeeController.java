37f48b2c79ae79a35f88b0d136fe8f349309e5dd
Leonardo José Nunes <leonardo.ferax@gmail.com>
2023-02-09 16:53:20 -0300

    Add controller save Employee.

--- src/main/java/ipmanager/controller/EmployeeController.java
+++ src/main/java/ipmanager/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ipmanager.model.Employee;
@@ -16,16 +17,16 @@ import ipmanager.repository.EmployeeRepository;
 public class EmployeeController {
 	@Autowired
 	private EmployeeRepository empRepo;
-	
+
 	@GetMapping("")
 	public String displayEmployees(Model model) {
 		List<Employee> employees = empRepo.findAll();
-		
-		model.addAttribute("employeeList" ,employees);
-		
+
+		model.addAttribute("employeeList", employees);
+
 		return "employees/list-employees";
 	}
-	
+
 	@GetMapping("/new")
 	public String Employee(Model model) {
 
@@ -36,5 +37,12 @@ public class EmployeeController {
 		return "employees/new-employee";
 	}
 
-	
+	@PostMapping("/save")
+	public String createEmployee(Employee employee, Model model) {
+		empRepo.save(employee);
+
+		// use a redirect to prevent duplicate submissions
+		return "redirect:/employees";
+	}
+
 }
