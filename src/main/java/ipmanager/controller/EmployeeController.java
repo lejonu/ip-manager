e484514bdc8d7f02767f6697e0bdc0425e63e069
Leonardo José Nunes <leonardo.ferax@gmail.com>
2023-02-10 12:22:51 -0300

    Delete functionality to Employees.

--- src/main/java/ipmanager/controller/EmployeeController.java
+++ src/main/java/ipmanager/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -42,6 +43,17 @@ public class EmployeeController {
 		empRepo.save(employee);
 
 		// use a redirect to prevent duplicate submissions
+		return "redirect:/employees";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String deleteEmployee(@PathVariable("id") long id, Model model) {
+		
+		Employee employee = empRepo.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Invalid employee id"));
+		
+		empRepo.delete(employee);
+		
 		return "redirect:/employees";
 	}
 
